885d9bb083baffb768191d9868492a0b3c880670
Новокшонов Михаил Васильевич <mvnovokshonov@edu.hse.ru>
2025-03-08 22:52:19 +0300

    demo appv

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,71 +1,18 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+// app/_layout.tsx
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import React, { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-import AppProvider from '../context/AppProvider';
-import { EventsProvider } from '@/context/EventsContext';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(drawer)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import React from 'react';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) {
-      throw error;
-    }
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <EventsProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <AppProvider>
-          <Stack>
-            <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="(modal)"
-                options={{
-                  presentation: 'modal',
-                  headerShown: false,
-                }}
-            />
-            <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-          </Stack>
-        </AppProvider>
-      </ThemeProvider>
-    </EventsProvider>
-  );
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <Stack>
+                {/* По умолчанию открывается экран входа (index.tsx) */}
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                {/* Экран календаря */}
+                <Stack.Screen name="calendar" options={{ headerShown: false }} />
+            </Stack>
+        </>
+    );
 }
